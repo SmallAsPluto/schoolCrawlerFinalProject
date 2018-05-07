1af94d756286531d2756b5af8961971a14b3488f
Max Dayley <maxdayley@ad.weber.edu>
2018-05-07 09:21:28 -0600

    Main Game alpha 0.0.1

---
+++ School Crawler/Assets/scripts/CUBE.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CUBE : MonoBehaviour {
+	public float distToGround = 0.5f;
+	public float speed = 10f;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+
+		transform.Translate (Input.GetAxis ("Horizontal")*Time.deltaTime,0f,Input.GetAxis ("Vertical")*Time.deltaTime);
+
+
+	}
+
+	void Fixedupdate () {
+		Debug.Log (Physics.Raycast (transform.position, Vector3.down, distToGround));
+
+
+	}
+}
