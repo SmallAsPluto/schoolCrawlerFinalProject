3eaea0c960e5c36fec9f966369fcd46875e2f651
maxdayley <36927346+maxdayley@users.noreply.github.com>
2018-05-09 09:03:07 -0600

    School Crawler alpha 0.0.01

---
+++ School Crawler/Assets/controllers/enemycontroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemycontroller : MonoBehaviour {
+
+    public float lookRaduis = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+  
+		
+	}
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRaduis);
+    }
+}
