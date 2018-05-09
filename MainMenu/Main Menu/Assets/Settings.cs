87312f33ec0a725f5b494a631d5584fe9f705909
CheekyCarp <36927354+CheekyCarp@users.noreply.github.com>
2018-05-09 09:00:45 -0600

    Main Menu
    Working
    - Volume
    - Play Button
    - Quit Button
    - Options Button
    
    Not Working
    - Fullscreen Toggle

--- MainMenu/Main Menu/Assets/Settings.cs
+++ MainMenu/Main Menu/Assets/Settings.cs
@@ -1,16 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Settings : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public AudioMixer audioMixer;
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
 }
